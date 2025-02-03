862ec66614f9246ad1204044973b23925ae443b2
Chandrasekhar174 <chandrasekharsahu174@gmail.com>
2025-02-03 16:51:21 +0530

    add test file

---
+++ Drill1/test.mjs
@@ -0,0 +1,15 @@
+import * as utility from './person.mjs';
+console.log(utility.getEmailId(utility.arrayOfObjects));
+console.log(utility.getAge(utility.arrayOfObjects,30));
+console.log(utility.getName(utility.arrayOfObjects));
+console.log(utility.getNameandCity(utility.arrayOfObjects));
+console.log(utility.getAllAge(utility.arrayOfObjects));
+console.log(utility.getFirstHobby(utility.arrayOfObjects));
+console.log(utility.getNameandEmail(utility.arrayOfObjects));
+console.log(utility.getCityandCountry(utility.arrayOfObjects));
+
+
+
+
+
+
