8be73e57c55d8f59178e1d59f9e976631277071d
Chandrasekhar174 <chandrasekharsahu174@gmail.com>
2025-02-07 16:51:07 +0530

    drill4

--- Drill3/test.mjs
+++ Drill3/test.mjs
@@ -1,8 +1,8 @@
 import * as utility from './invertory.mjs';
 
   console.log(utility.findCarinfoById(34));
-//   console.log(utility.lastCarInfo());
-//   console.log(utility.sortByCarModel());
-//   console.log(utility.getAllYear());
-//   console.log(utility.getOlderCarInfo(2000));
-//   console.log(JSON.stringify(utility.getInfoByCarMake("BMW","audi")));
+  console.log(utility.lastCarInfo());
+  console.log(utility.sortByCarModel());
+  console.log(utility.getAllYear());
+  console.log(utility.getOlderCarInfo(2000));
+  console.log(JSON.stringify(utility.getInfoByCarMake("BMW","audi")));
---
+++ Drill4/test.mjs
@@ -0,0 +1,15 @@
+import * as utility from './item.mjs'
+
+
+utility.each(utility.items, (num, index) => {
+  console.log(`Element at index ${index}: ${num}`);
+});
+
+const getElement=utility.map(utility.items,(num)=>num);
+console.log(getElement);
+
+console.log(utility.reduce(utility.items,function(num){return num==3}));
+
+const t=utility.filter(utility.items,num=>num%2===0);
+console.log(t);
+console.log(utility.flatten(utility.nestedArray));
