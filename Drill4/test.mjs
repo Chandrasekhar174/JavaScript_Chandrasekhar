b2a3fec90aeadc6a25bbc2dee7270d279e88e652
Chandrasekhar174 <chandrasekharsahu174@gmail.com>
2025-02-13 00:12:46 +0530

    ADD IPL PROJECT

--- Drill4/test.mjs
+++ Drill4/test.mjs
@@ -1,15 +1,15 @@
 import * as utility from './item.mjs'
 
 
-utility.each(utility.items, (num, index) => {
-  console.log(`Element at index ${index}: ${num}`);
-});
+// utility.each(utility.items, (num, index) => {
+//   console.log(`Element at index ${index}: ${num}`);
+// });
 
-const getElement=utility.map(utility.items,(num)=>num);
-console.log(getElement);
+// const getElement=utility.map(utility.items,(num)=>num);
+// console.log(getElement);
 
-console.log(utility.reduce(utility.items,function(num){return num==3}));
+// console.log(utility.reduce(utility.items,function(num){return num==3}));
 
-const t=utility.filter(utility.items,num=>num%2===0);
+const t=utility.filter(utility.items,num=>num>2);
 console.log(t);
-console.log(utility.flatten(utility.nestedArray));
+// console.log(utility.flatten(utility.nestedArray));
